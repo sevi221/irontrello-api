610b4245539f33d46e74fb0b95e8d894f30237bf
sevi221 <sevilaura221@gmail.com>
2018-03-17 14:59:31 +0100

    first commit

--- models/card.model.js
+++ models/card.model.js
@@ -9,8 +9,22 @@ const cardSchema = new mongoose.Schema({
   position: Number,
   list: {
     type: String,
-    enum: ['ToDo', 'WorkInProgress', 'Done']
+    enum: ['ToDo', 'WorkInProgress', 'Done'],
+    required: true
   }
+  }, {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at"
+    },
+    toJSON: {
+      transform: (doc, ret) => {
+        ret.id = doc._id;
+        delete ret._id;
+        delete ret.__v;
+        return ret;
+      }
+    }
 });
 
 module.exports = mongoose.model('Card', cardSchema);
--- routes/cards.routes.js
+++ routes/cards.routes.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 const cardsController = require('../controllers/cards.controller');
 const secureMiddleware = require('../middleware/secure.middleware');
+const checkId = require('../middleware/validateId.middleware');
+
 
 router.get('/', secureMiddleware.isAuthenticated, cardsController.index);
-router.get('/:id', secureMiddleware.isAuthenticated, cardsController.show);
+router.get('/:id', secureMiddleware.isAuthenticated, checkId.checkId, cardsController.show);
 router.post('/', secureMiddleware.isAuthenticated, cardsController.create);
-router.put('/:id', secureMiddleware.isAuthenticated, cardsController.update);
-router.delete('/:id', secureMiddleware.isAuthenticated, cardsController.destroy);
+router.put('/:id', secureMiddleware.isAuthenticated, checkId.checkId, cardsController.update);
+router.delete('/:id', secureMiddleware.isAuthenticated, checkId.checkId, cardsController.destroy);
 
 module.exports = router;
